b9aac244d0115d08ff152427152792ef625cc3fe
TJ <xxterrelljacksonxx@gmail.com>
2023-04-25 00:02:02 -0400

    added sticky nav

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+let nav = document.querySelector('nav')
+let logo = document.querySelector('.logo')
+let social = document.querySelector('.social')
 
+
+
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 50) {
+        nav.style.backgroundColor = 'black'
+        nav.style.padding = '1rem'
+        nav.style.justifyContent = 'center'
+        logo.style.display = 'none'
+        social.style.display = 'none'
+    } else {
+        nav.style.backgroundColor = 'transparent'
+        logo.style.display = ''
+        social.style.display = ''
+    }
+}
