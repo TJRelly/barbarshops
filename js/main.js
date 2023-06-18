45b0f2bec7053773d327f79604ab524f25828bf1
TJ <xxterrelljacksonxx@gmail.com>
2023-06-18 15:07:38 -0400

    meet update

--- js/main.js
+++ js/main.js
@@ -1,64 +1,39 @@
-const nav = document.querySelector('nav')
-const showNav = document.querySelector('show-nav')
-const logo = document.querySelector('.logo')
-const social = document.querySelector('.social')
-const burger = document.querySelector('.hamburger-menu')
-const navLeft = document.querySelector('.navbar-left')
+const nav = document.querySelector("nav")
+const navLeft = document.querySelector(".navbar-left")
+const menuBtn = Array.from(document.querySelectorAll(".menu-btn"))
+const members = Array.from(document.querySelectorAll(".member"))
 
-const menuBtn = Array.from(document.querySelectorAll(".menu-btn"));
-const navUl = document.querySelector(".nav ul")
+for (const btn of menuBtn) {
+  btn.onclick = toggleMenu
+}
 
-for (btn in menuBtn) {
-    menuBtn[btn].onclick = toggleMenu;
+for (const member of members) {
+  const memberImg = member.querySelector("img")
+  const socials = member.querySelector(".member-social")
+
+  member.onmouseover = () => toggleSocials(memberImg, socials)
+  member.onmouseleave = () => toggleSocialsOff(memberImg, socials)
+}
+
+function toggleSocials(memberImg, socials) {
+  socials.style.display = "flex"
+  memberImg.style.opacity = "25%"
+}
+
+function toggleSocialsOff(memberImg, socials) {
+  socials.style.display = ""
+  memberImg.style.opacity = ""
 }
 
 function toggleMenu() {
-    nav.classList.toggle("show-nav")
-    navLeft.classList.toggle("overlay")
+  nav.classList.toggle("show-nav")
+  navLeft.classList.toggle("overlay")
 }
 
-let media800 = window.matchMedia("(max-width: 875px)")
-myFunction(media800) // Call listener function at run time
-media800.addListener(myFunction) // Attach listener function on state changes
-
-function myFunction(media800) {
-    if (media800.matches) { // If media query matches
-        window.onscroll = scrollSmall
-    } else {
-        window.onscroll = scrollBig
-    }
-}
-
-function scrollBig() {
-    if (document.documentElement.scrollTop > 50) {
-        nav.style.backgroundColor = 'black'
-        nav.style.justifyContent = 'center'
-        nav.style.padding = '1rem'
-        logo.style.display = 'none'
-        social.style.display = 'none'
-    } else {
-        nav.style.backgroundColor = ''
-        nav.style.padding = ''
-        nav.style.justifyContent = ''
-        logo.style.display = ''
-        social.style.display = ''
-    }
-}
-
-function scrollSmall() {
-    if (document.documentElement.scrollTop > 50) {
-        nav.style.backgroundColor = 'black'
-        nav.style.padding = '1rem 2rem'
-        showNav.style.backgroundColor = 'black'
-        showNav.style.justifyContent = 'end'
-        logo.style.display = 'none'
-        social.style.display = 'none'
-    } else {
-        nav.style.backgroundColor = ''
-        showNav.style.backgroundColor = ''
-        showNav.style.padding = ''
-        showNav.style.justifyContent = ''
-        logo.style.display = ''
-        social.style.display = ''
-    }
-}
+document.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    nav.classList.add("scrolled")
+  } else {
+    nav.classList.remove("scrolled")
+  }
+})
